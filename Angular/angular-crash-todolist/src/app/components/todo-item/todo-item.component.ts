828b6b1f5451c4a020685f3dc55bcae09a042d48
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-13 13:59:32 -0700

    added todos component and a todo item component

---
+++ Angular/angular-crash-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { splitClasses } from '@angular/compiler';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Set Dyanmic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+}
--- Angular/angular-crash-todolist/src/app/components/todos/todos.component.ts
+++ Angular/angular-crash-todolist/src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo'
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos:Todo[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      },
+    ]
   }
 
 }
