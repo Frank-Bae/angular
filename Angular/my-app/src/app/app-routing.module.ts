9cd87fd3a753ef86468a08908036b928c8d29aaf
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-13 23:35:47 -0700

    added routers

--- Angular/my-app/src/app/app-routing.module.ts
+++ Angular/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// This is where you want to import the components you will be using to route
+import { HomeComponent } from './home/home.component'
+import { ListComponent } from './list/list.component'
 
-const routes: Routes = [];
+// You would use this to connect the routes you want to connect components
+// the path that has an empty string will be the one that loads first
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
