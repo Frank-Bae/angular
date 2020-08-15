1fd965b7564f4e03680d6a34db820bc10b5e351a
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-14 18:41:09 -0700

    added http lit

---
+++ Angular/my-app/src/app/http.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  myMethod() {
+    return console.log('yo')
+  }
+
+  getBeer() {
+    return this.http.get('https://api.openbrewerydb.org/breweries')
+  }
+}
--- Angular/my-app/src/app/list/list.component.ts
+++ Angular/my-app/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: Object;
 
+  constructor(private _http: HttpService) { }
+
+  //Whatever is loaded in ngInit will run when the component is loaded
+  //Similar to componentDidMount
   ngOnInit(): void {
+    this._http.myMethod();
+    this._http.getBeer().subscribe(data => {
+      this.brews = data
+      console.log(this.brews)
+    });
   }
 
 }
