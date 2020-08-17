214c7e98ee05411a239f7c67ac8f182a66817ae3
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-17 15:54:38 -0700

    last app

---
+++ Angular/last-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//type script knows where to find server component
+import { ServerComponent } from './server/server.component'
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //whenver you aadd a component it must go in the decleration
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  //the bootstrap indicates to Angular of which module to look for
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
