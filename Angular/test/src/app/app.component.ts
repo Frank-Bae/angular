a2007a5e3bd7dbbda30eba8b8a521bf4a37426c3
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-19 11:40:05 -0700

    completed all syntax

--- Angular/test/src/app/app.component.ts
+++ Angular/test/src/app/app.component.ts
@@ -6,12 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
-  data1: string = 'one way';
-  data2: string = '';
-  serverCreated = false;
+  name: string = 'frank';
+  myId = "testId";
+  isDisabled = true;
 
-  onCreateServer() {
-    this.serverCreated = true;
+  //class binding
+  successClass = 'text-success';
+  hasError = true;
+  isSpecial = true;
+  messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
   }
+
+  highlightColor = "orange";
+  titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+
+  title = 'test';
+
+  getInterpolation() {
+    return "hello " + this.name;
+  }
+
+  //event binding
+  onClick(event) {
+    console.log(event);
+    this.greeting = event.type;
+  }
+  greeting = "";
+
+  //template reference variable
+  logMessage(value){
+    console.log(value)
+  }
+
+  //Two Way Binding
+  names = "";
+
+  //Structural Direct
+  displayName = true;
+  color = "blue";
+
+  colors = ["red", "blue", "green", "yellow"]
 }
