fbd40525d9b22e0f73347c8b7fab17c570aa6832
Frank <50495924+Bae26@users.noreply.github.com>
2020-08-24 21:03:27 -0700

    completed event binding

--- angular-udemy/basic/src/app/servers/servers.component.ts
+++ angular-udemy/basic/src/app/servers/servers.component.ts
@@ -11,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'no server was created';
+  serverName = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
-
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created';
+  }
+  onUpdateServerName(event: any) {
+    console.log(event)
+    this.serverName = event.target.value;
+  }
 }
