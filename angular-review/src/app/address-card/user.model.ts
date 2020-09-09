7243fea9f77e2c73e786669f3adf976cd1a06f16
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-08 18:01:57 -0700

    rearrangin file

--- angular-review/src/app/address-card/user.model.ts
+++
@@ -1,9 +0,0 @@
-  //create a typescript class how the user object should be.
-  // use this to gether users input
-
-export class User {
-  name: string;
-  title: string;
-  address: string;
-  phone: string[];
-}
