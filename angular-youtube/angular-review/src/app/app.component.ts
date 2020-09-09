01e97817d7297819b08db9fcabe706ec6b5e9400
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-08 17:55:07 -0700

    moved around files

---
+++ angular-youtube/angular-review/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  //now that it has all the properties and values, it can be passed to a different
+  //component, similar to prop. (2)
+  user: User;
+  inputText: string = "Initial value";
+
+  constructor() {
+    // allowing the user variable to get its blue print or instantiate
+    // from the user.model.ts class (1)
+    this.user = new User();
+    this.user.name = "frank";
+    this.user.title = "developer";
+    this.user.address = "222 goodee st";
+    this.user.phone = [
+      '333-444-2943',
+      '333-444-2323'
+    ]
+  }
+}
