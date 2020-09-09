b58873bb90d7e76da3247403f07e55d4b96b4b3e
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-08 23:48:37 -0700

    understood how to create a sperate module and use that module within the app module

---
+++ angular-youtube/angular-advanced/src/app/view/view.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewComponentComponent } from './view-component/view-component.component';
+
+
+
+@NgModule({
+  declarations: [ViewComponentComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ViewComponentComponent
+  ]
+})
+export class ViewModule { }
