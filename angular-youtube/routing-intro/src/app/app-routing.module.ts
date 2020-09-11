e9fc99eb57b65c2e2d837a7c76f99b9b03ae2b8d
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-11 16:43:16 -0700

    successfully understand basic routing and setup

--- angular-youtube/routing-intro/src/app/app-routing.module.ts
+++ angular-youtube/routing-intro/src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { SettingsComponent } from './settings/settings.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 //this is what configures your routes
 const routes: Routes = [
   //the path is the url path
+  // when path is empty, that is the first thing user will see. You are
+  // redirecting the view to the home path. It will automatically direct
+  // to home.
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'settings', component: SettingsComponent },
+  //this is a wild card where if the path doesn't match any of the paths established
+  //it will change the view to a custom error component that you created.
+  //no matter what the url is map it to the component. it is not an angular error
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- angular-youtube/routing-intro/src/app/app.module.ts
+++ angular-youtube/routing-intro/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SettingsComponent
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
