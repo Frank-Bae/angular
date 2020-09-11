bae062e65e3096afbe10e1c8f64e8f9743f3a264
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-11 15:39:08 -0700

    understood how to build angular production

--- angular-youtube/angular-advanced/src/main.ts
+++ angular-youtube/angular-advanced/src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+// This is to build the site into production so that you can host it on web
+// cli command: ng build --prod
+// cli command: http-server dist/angular/advance
+// Now you have access to the index.html
 if (environment.production) {
   enableProdMode();
 }
