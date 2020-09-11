be54161c392a0465614a0884d023735e1727536d
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-11 16:09:19 -0700

    added files for routing

--- angular-youtube/routing-intro/src/app/app-routing.module.ts
+++ angular-youtube/routing-intro/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { SettingsComponent } from './settings/settings.component'
 
-const routes: Routes = [];
+//this is what configures your routes
+const routes: Routes = [
+  //the path is the url path
+  { path: 'home', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
