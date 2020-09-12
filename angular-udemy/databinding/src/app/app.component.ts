0600ec81c8c0c1b4df9e0982b4a68d20bb7c112c
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-11 20:07:11 -0700

    redo

--- angular-udemy/databinding/src/app/app.component.ts
+++ angular-udemy/databinding/src/app/app.component.ts
@@ -9,5 +9,19 @@ export class AppComponent {
   //this is being passed to the server element component within the view page
   serverElements = [{type: 'server', name: 'Typescript', content: 'Just a test'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
