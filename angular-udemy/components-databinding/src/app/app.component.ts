d4ee16f7b08fc056b2316aa0b95616cbd07f5e95
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-13 16:31:24 -0700

    completed databinding course

--- angular-udemy/components-databinding/src/app/app.component.ts
+++ angular-udemy/components-databinding/src/app/app.component.ts
@@ -30,4 +30,12 @@ export class AppComponent {
       content: bluePrintData.serverContent
     })
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
--- angular-udemy/components-databinding/src/app/server-element/server-element.component.ts
+++ angular-udemy/components-databinding/src/app/server-element/server-element.component.ts
@@ -1,20 +1,64 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges,
+   DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,
+    AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,
+ OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+ AfterViewInit, AfterViewChecked, OnDestroy {
   // With the input in place, we are able to access this property to the world
   // any parent or component is now able to bind to element
   // this is an alias, if you want to bind to this property, it has to be
   // srvElement. We are passing data from the parent to the child
   @Input('srvElement') element: {type: string, name: string, content: string}
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
-  constructor() { }
+
+  // lifecycle hook
+  constructor() {
+    console.log('constructor called')
+   }
+
+   ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!')
+    console.log(changes)
+   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called')
+    console.log('Text Content ' + this.header.nativeElement.textContent )
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent)
   }
 
+  ngDoCheck() {
+    console.log('ngDoCheck called')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called')
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent)
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called')
+    console.log('Text Content ' + this.header.nativeElement.textContent)
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterContentChecked called')
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called')
+  }
 }
