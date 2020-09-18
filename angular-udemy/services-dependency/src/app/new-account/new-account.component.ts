0d9558a698eed48faa649ecdbaaf321ced1d43ed
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-17 21:29:26 -0700

    completed services course

--- angular-udemy/services-dependency/src/app/new-account/new-account.component.ts
+++ angular-udemy/services-dependency/src/app/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
+
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // this tells angular we want an instance of hte LoggingService class from service.ts
+  // In addition, within the components, you must provide a providers within @Component
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus)
   }
 }
