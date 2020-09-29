f6e6f9b47f4c28656f7c4a2820511a63b80be5ff
Frank <50495924+Bae26@users.noreply.github.com>
2020-09-29 14:27:36 -0700

    completed once again part 4

---
+++ angular-hero/practice/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { MessageComponent } from './message/message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-hero/practice/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'test1' },
+  { id: 2, name: 'test2' },
+  { id: 3, name: 'test3' },
+  { id: 4, name: 'test4' },
+  { id: 5, name: 'test5' },
+]
