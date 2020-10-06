6fdda888a5b73d3f25d33b3e78ec00ba77cb92ca
Frank <50495924+Bae26@users.noreply.github.com>
2020-10-06 13:03:44 -0700

    restarting file

--- angular-hero/practice/src/app/mock-heroes.ts
+++
@@ -1,9 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-  { id: 1, name: 'testing1' },
-  { id: 2, name: 'testing2' },
-  { id: 3, name: 'testing3' },
-  { id: 4, name: 'testing4' },
-  { id: 5, name: 'testing5' },
-];
