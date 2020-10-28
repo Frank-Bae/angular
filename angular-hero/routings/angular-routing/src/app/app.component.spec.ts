4df357ccea0494f95bfece018cd5526462f5ffbe
Frank <50495924+Bae26@users.noreply.github.com>
2020-10-28 16:12:19 -0700

    added new file

--- angular-hero/routings/angular-routing/src/app/app.component.spec.ts
+++ angular-hero/routings/angular-routing/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-router'`, () => {
+  it(`should have as title 'angular-routing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-router');
+    expect(app.title).toEqual('angular-routing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-router app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-routing app is running!');
   });
 });
