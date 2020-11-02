b3c2ca40733a4d571f9d1e8d6664b6398c1ecc2e
Frank <50495924+Bae26@users.noreply.github.com>
2020-11-02 13:49:07 -0800

    starting over

--- angular-hero/updated-hero-http/src/app/hero.ts
+++
@@ -1,8 +0,0 @@
-// interface is only a contract that does not instnatiate a new class
-// so only checks to see if the properties are there or not
-// class does the same thing as interface but is able to instantiate a new object
-// based on the blueprint
-export class Hero {
-  id: number;
-  title: string;
-}
